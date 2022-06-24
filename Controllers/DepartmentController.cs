95459b92a44feba1691ea05829161cbfa5192799
Shivansh Mittal <81565355+Shivansh-Mittal@users.noreply.github.com>
2022-06-24 19:53:53 +0530

    Add files via upload

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuestDbPOC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : BaseController
+    {
+        [HttpGet("[action]")]
+        public IActionResult GetDepartments()
+        {
+            var depart = db.Departments.ToList();
+            return Ok(depart);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Department model)
+        {
+            if (model.DepartmentId == 0)
+            {
+                await db.Departments.AddAsync(model);
+            }
+            else
+            {
+                db.Departments.Update(model);
+            }
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Department> Delete(int id)
+        {
+            var res = db.Departments.SingleOrDefault(x => x.DepartmentId == id);
+            if (res == null)
+            {
+                return NotFound();
+            }
+            db.Departments.Remove(res);
+            db.SaveChanges();
+            return res;
+        }
+    }
+}
---
+++ Models/ApiSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace QuestDbPOC.Models
+{
+    public class ApiSettings
+    {
+        public string WebUrl { get; set; }
+        public string EmailImageUrl { get; set; }
+        public string WebChangePasswordUrl { get; set; }
+        public string ConfirmEmailUrl { get; set; }
+    }
+}
