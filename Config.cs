2d9c6289c21277401e75e3d3cfd8d8bd599e87c8
Shivansh Mittal <81565355+Shivansh-Mittal@users.noreply.github.com>
2022-06-24 19:52:27 +0530

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,14 @@
+﻿namespace QuestDbPOC
+{
+    public class Config
+    {
+        public Config()
+        {
+        }
+        public static string ConnectionString = string.Empty;
+        public static ApiSettings ApiSettings;
+
+
+        public static string AdminUsers { get; set; }
+    }
+}
---
+++ JsonExceptionFilter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace QuestDbPOC
+{
+    public class JsonExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var result = new ObjectResult(context.Exception);
+            result.StatusCode = 500;
+            context.Result = result;
+        }
+    }
+}
